c340d666a8c666e4993f253f05157f2b9bfa857b
Prathmesh Ravindra Salunkhe <salunkheprathmesh0@gmail.com>
2026-05-06 00:02:00 +0530

    added hf_token while downloading, fixed

--- code/utils/data.py
+++ code/utils/data.py
@@ -1,17 +1,20 @@
 from huggingface_hub import login, upload_folder, snapshot_download
-from config import HF_TOKEN
+import os
+
+token = os.getenv("HF_TOKEN")
 
 def upload_data():
-    login()
+    login(token=token)
     # Push your dataset files
     upload_folder(folder_path="./data", repo_id="prathmesh796/supportAgent.ai", repo_type="dataset")
 
 def download_data():
     # Download your dataset files
+
     local_path = snapshot_download(
         repo_id="prathmesh796/supportAgent.ai",
         repo_type="dataset",              
         local_dir="./data",  
-        token=HF_TOKEN
+        token=token
     )
     print(local_path)
